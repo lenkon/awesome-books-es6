296d73ae55b2a5f32384e70f62bf3bd68c9bdf82
lenkon <inoulenkon@gmail.com>
2023-01-24 15:26:56 +0530

    Fix linter errors and correct module paths

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-
-import showDate from '/modules/show_date.js';
-import { libraryData } from '/modules/event_handler.js';
+import showDate from './modules/show_date.js';
+import showContent from './modules/show_content.js';
+import Library from './modules/library.js';
 
 setInterval(showDate, 5000);
 
 document.getElementById('addbook').classList.add('show-none');
 document.getElementById('contact').classList.add('show-none');
 
+const menuItems = document.querySelectorAll('.menu-item');
+const libraryData = new Library();
 libraryData.showSavedData();
+
+libraryData.formInput.addEventListener('submit', libraryData.addBookData.bind(libraryData));
+menuItems.forEach((menuItem) => menuItem.addEventListener('click', showContent));
